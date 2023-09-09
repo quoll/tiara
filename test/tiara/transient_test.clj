2882cd5cbcba2ad4af4cd10dd0492c149cee6b90
Paula Gearon <pgearon@imohealth.com>
2023-09-08 22:01:22 -0400

    Full transient testing

--- test/tiara/transient_test.clj
+++ test/tiara/transient_test.clj
@@ -184,66 +184,60 @@
       (is (= k20 (seq (into (apply ordered-set k10)
                             (drop 10 k20))))))))
 
-;;;;;;;;;;;;;;
+(deftest test-set-internal-mutability
+  (testing "That adding small amounts of data in the set do not change the object"
+    (let [ts (transient EMPTY_SET)]
+      (conj! ts :a)
+      (conj! ts :b)
+      (conj! ts :c)
+      (is (= #{:a :b :c} (persistent! ts))))))
 
-(comment
-(deftest test-internal-mutability
-  (testing "That small amounts of data do not change the object"
-    (let [tm (transient EMPTY_MAP)]
-      (assoc! tm :a 1)
-      (assoc! tm :b 2)
-      (conj! tm [:c 3])
-      (is (= {:a 1 :b 2 :c 3} (persistent! tm))))))
+(deftest test-set-without
+  (testing "without removes data from sets correctly"
+    (is (= #{:a :c} (-> (ordered-set :a :b :c)
+                        transient
+                        (disj! :b)
+                        persistent!)))
+    (let [rkeys (take 550 (shuffle k1100))
+          result-set (-> (apply ordered-set k1100)
+                         transient
+                         (#(apply disj! % rkeys))
+                         persistent!)]
+      (is (= (apply disj (apply ordered-set k1100) rkeys) result-set))
+      (is (= (remove (set rkeys) k1100)
+             (seq result-set))))))
 
-(deftest test-without
-  (testing "without removes data correctly"
-    (is (= {:a 1 :c 3} (-> (ordered-map :a 1 :b 2 :c 3)
-                           transient
-                           (dissoc! :b)
-                           persistent!)))
-    (let [rng (make-kv-range 1100)
-          rkeys (map make-key (take 550 (shuffle (range 1100))))]
-      (is (= (apply dissoc (apply ordered-map rng) rkeys)
-             (-> (apply ordered-map rng)
-                 transient
-                 (#(apply dissoc! % rkeys))
-                 persistent!))))))
-
-(deftest test-count
-  (testing "count operation on transient maps"
-    (is (= 1 (count (transient (ordered-map :a 1)))))
-    (is (= 3 (count (transient (ordered-map :a 1 :b 2 :c 3)))))
-    (let [rng (make-kv-range 1100)
-          rkeys (map make-key (take 550 (shuffle (range 1100))))]
+(deftest test-set-count
+  (testing "count operation on transient sets"
+    (is (= 1 (count (transient (ordered-set :a)))))
+    (is (= 3 (count (transient (ordered-set :a :b :c)))))
+    (let [rkeys (take 550 (shuffle k1100))]
       (is (= 550
-             (-> (apply ordered-map rng)
+             (-> (apply ordered-set k1100)
                  transient
-                 (#(apply dissoc! % rkeys))
+                 (#(apply disj! % rkeys))
                  count))))))
 
-(deftest test-in-and-out
-  (testing "checking if data is consistent if swapping between additions and removals"
-    (is (= {:a 1 :c 3 :d 4} (-> (ordered-map :a 1 :b 2 :c 3)
-                                transient
-                                (dissoc! :b)
-                                (assoc! :d 4)
-                                persistent!)))
-    (let [rng (make-kv-range 1100)
-          reordered-vals (take 550 (shuffle (range 1100)))
-          rkeys (map make-key reordered-vals)
-          ikvs (take 550 (interleave rkeys reordered-vals))
-          o1 (-> (apply ordered-map rng)
-                 (#(apply dissoc % rkeys))
-                 (#(apply assoc % ikvs)))
-          to1 (-> (apply ordered-map rng)
+(deftest test-set-in-and-out
+  (testing "checking if set data is consistent if swapping between additions and removals"
+    (is (= #{:a :c :d} (-> (ordered-set :a :b :c)
+                           transient
+                           (disj! :b)
+                           (conj! :d)
+                           persistent!)))
+    (let [rkeys (take 550 (shuffle k1100))
+          ikeys (take 275 rkeys)
+          o1 (-> (apply ordered-set k1100)
+                 (#(apply disj % rkeys))
+                 (#(apply conj % ikeys)))
+          to1 (-> (apply ordered-set k1100)
                   transient
-                  (#(apply dissoc! % rkeys))
-                  (#(apply assoc! % ikvs))
+                  (#(apply disj! % rkeys))
+                  (#(reduce conj! % ikeys))
                   persistent!)]
       (is (= o1 to1))
+      (is (= (concat (remove (set rkeys) k1100) ikeys) (seq to1)))
       (is (= 825 (count to1)))
       (is (= 825 (count (seq to1)))))))
-  )
-
 
 ;; TIARA Is A Recursive Acronym
