096a3a783d1bd2dcd5299c0bba198b81cb1827de
Paula Gearon <gearon@gmail.com>
2025-10-09 20:27:07 +0200

    Initial compile

--- src/tiara/data.clj
+++ src/tiara/data.clj
@@ -4,7 +4,7 @@
   (:import [clojure.lang IFn APersistentMap APersistentSet MapEntry
             IPersistentMap IPersistentSet IHashEq IObj MapEquivalence
             IEditableCollection ITransientVector ITransientMap ITransientSet]
-           [java.util Map Set Collection]))
+           [java.util Map Map$Entry Set Collection]))
 
 (def ^:const magic 7)
 (def ^:const set-magic 11)
@@ -27,13 +27,13 @@
 
 (deftype VecMap [lst idx ^int _hash]
   IFn
-  (invoke [this k] (indirect idx lst k))
-  (invoke [this k not-found] (indirect-nf idx lst k not-found))
-  (invoke [this a b & rest] (throw (UnsupportedOperationException.)))
-  (applyTo [this s] (case (count s)
-                      1 (indirect idx lst (first s))
-                      2 (indirect-nf idx lst (first s) (second s))
-                      (throw (clojure.lang.ArityException. (count s) "Map.invoke"))))
+  (invoke [_ k] (indirect idx lst k))
+  (invoke [_ k not-found] (indirect-nf idx lst k not-found))
+  (invoke [_ _ _ & _] (throw (UnsupportedOperationException.)))
+  (applyTo [_ s] (case (count s)
+                   1 (indirect idx lst (first s))
+                   2 (indirect-nf idx lst (first s) (second s))
+                   (throw (clojure.lang.ArityException. (count s) "Map.invoke"))))
 
   IPersistentMap
   (assoc [this k v]
@@ -56,22 +56,22 @@
                        (range (inc split) (count lst)))
                0)
       this))
-  (iterator [this] (.iterator ^Collection lst))
-  (containsKey [this k] (contains? idx k))
-  (entryAt [this k] (when-let [n (idx k)] (nth lst n)))
-  (count [this] (count lst))
+  (iterator [_] (.iterator ^Collection lst))
+  (containsKey [_ k] (contains? idx k))
+  (entryAt [_ k] (when-let [n (idx k)] (nth lst n)))
+  (count [_] (count lst))
   (cons [this [k v]] (.assoc this k v))
-  (empty [this] (VecMap. [] (.withMeta ^IObj {} (.meta ^IObj idx)) 0))
+  (empty [_] (VecMap. [] (.withMeta ^IObj {} (.meta ^IObj idx)) 0))
   (equiv [this o]
     (if (instance? IPersistentMap o)
       (and (instance? MapEquivalence o) (APersistentMap/mapEquals this o))
       (APersistentMap/mapEquals this o)))
-  (seq [this] (seq lst))
-  (valAt [this k] (indirect idx lst k))
-  (valAt [this k not-found] (indirect-nf idx lst k not-found))
+  (seq [_] (seq lst))
+  (valAt [_ k] (indirect idx lst k))
+  (valAt [_ k not-found] (indirect-nf idx lst k not-found))
 
   IEditableCollection
-  (asTransient [this] (transient-ordered-map lst idx))
+  (asTransient [_] (transient-ordered-map lst idx))
 
   IHashEq
   (hasheq [this]
@@ -80,24 +80,24 @@
       _hash))
   
   IObj
-  (withMeta [this meta] (VecMap. lst (.withMeta ^IObj idx meta) 0))
-  (meta [this] (.meta ^IObj idx))
+  (withMeta [_ meta] (VecMap. lst (.withMeta ^IObj idx meta) 0))
+  (meta [_] (.meta ^IObj idx))
 
   MapEquivalence
 
   Map
-  (clear [this] (throw (UnsupportedOperationException.)))
-  (containsValue [this v] (some #(= % v) (map val lst)))
+  (clear [_] (throw (UnsupportedOperationException.)))
+  (containsValue [_ v] (some #(= % v) (map val lst)))
   (equals [this o] (.equiv this o))
-  (get [this k] (indirect idx lst k))
-  (hashCode [this] (+ magic (hash lst)))
-  (isEmpty [this] (empty? lst))
-  (keySet [this] (set (map key lst)))
-  (size [this] (count lst))
-  (values [this] (map val lst))
-  (put [this k v] (throw (UnsupportedOperationException.)))
-  (putAll [this m] (throw (UnsupportedOperationException.)))
-  (remove [this k] (throw (UnsupportedOperationException.)))
+  (get [_ k] (indirect idx lst k))
+  (hashCode [_] (+ magic (hash lst)))
+  (isEmpty [_] (empty? lst))
+  (keySet [_] (set (map key lst)))
+  (size [_] (count lst))
+  (values [_] (map val lst))
+  (put [_ _ _] (throw (UnsupportedOperationException.)))
+  (putAll [_ _] (throw (UnsupportedOperationException.)))
+  (remove [_ _] (throw (UnsupportedOperationException.)))
 
   Object
   (toString [this] (clojure.lang.RT/printString this)))
@@ -149,21 +149,21 @@
                                   (dissoc! idx k)
                                   (range (inc split) (count plst)))))
       this))
-  (valAt [this key]
+  (valAt [_ key]
     (when-let [n (idx key)]
       (val (nth lst n))))
-  (valAt [this key not-found]
+  (valAt [_ key not-found]
     (if-let [n (idx key)]
       (val (nth lst n))
       not-found))
-  (count [this] (count lst))
-  (persistent [this] (VecMap. (.persistent lst) (.persistent idx) 0))
+  (count [_] (count lst))
+  (persistent [_] (VecMap. (.persistent lst) (.persistent idx) 0))
   (conj [this [k v]] (.assoc this k v))
 
   IFn
   (invoke [this k] (.valAt this k))
   (invoke [this k not-found] (.valAt this k not-found))
-  (invoke [this a b & rest] (throw (UnsupportedOperationException.)))
+  (invoke [_ _ _ & _] (throw (UnsupportedOperationException.)))
   (applyTo [this s] (case (count s)
                       1 (.valAt this (first s))
                       2 (.valAt this (first s) (second s))
@@ -177,38 +177,38 @@
 
 (deftype VecSet [^VecMap om ^int _hash]
   IFn
-  (invoke [this k] (.invoke om k))
-  (invoke [this k not-found] (.invoke om k not-found))
-  (invoke [this a b & rest] (throw (UnsupportedOperationException.)))
-  (applyTo [this s] (case (count s)
-                      1 (.invoke om (first s))
-                      2 (.invoke om (first s) (second s))
-                      (throw (clojure.lang.ArityException. (count s) "Set.invoke"))))
+  (invoke [_ k] (.invoke om k))
+  (invoke [_ k not-found] (.invoke om k not-found))
+  (invoke [_ _ _ & _] (throw (UnsupportedOperationException.)))
+  (applyTo [_ s] (case (count s)
+                   1 (.invoke om (first s))
+                   2 (.invoke om (first s) (second s))
+                   (throw (clojure.lang.ArityException. (count s) "Set.invoke"))))
 
   IPersistentSet
-  (disjoin [this k] (VecSet. (.without om k) 0))
-  (contains [this k] (.containsKey om k))
-  (get [this k] (.get om k))
-  (count [this] (.count om))
-  (cons [this o] (VecSet. (.assoc om o o) 0))
-  (empty [this] (VecSet. EMPTY_MAP 0))
+  (disjoin [_ k] (VecSet. (.without om k) 0))
+  (contains [_ k] (.containsKey om k))
+  (get [_ k] (.get om k))
+  (count [_] (.count om))
+  (cons [_ o] (VecSet. (.assoc om o o) 0))
+  (empty [_] (VecSet. EMPTY_MAP 0))
   (equiv [this o] (APersistentSet/setEquals this o))
-  (seq [this] (keys om))
+  (seq [_] (keys om))
 
   Set
-  (add [this e] (throw (UnsupportedOperationException.)))
-  (addAll [this c] (throw (UnsupportedOperationException.)))
-  (clear [this] (throw (UnsupportedOperationException.)))
-  (containsAll [this c] (every? om c))
+  (add [_ _] (throw (UnsupportedOperationException.)))
+  (addAll [_ _] (throw (UnsupportedOperationException.)))
+  (clear [_] (throw (UnsupportedOperationException.)))
+  (containsAll [_ c] (every? om c))
   (equals [this o] (APersistentSet/setEquals this o))
-  (hashCode [this] (+ set-magic (.hashCode om)))
-  (isEmpty [this] (.isEmpty om))
-  (iterator [this] (clojure.lang.SeqIterator. (keys om)))
-  (remove [this o] (throw (UnsupportedOperationException.)))
-  (removeAll [this c] (throw (UnsupportedOperationException.)))
-  (retainAll [this c] (throw (UnsupportedOperationException.)))
-  (size [this] (.count om))
-  (toArray [this] (clojure.lang.RT/seqToArray (keys om)))
+  (hashCode [_] (+ set-magic (.hashCode om)))
+  (isEmpty [_] (.isEmpty om))
+  (iterator [_] (clojure.lang.SeqIterator. (keys om)))
+  (remove [_ _] (throw (UnsupportedOperationException.)))
+  (removeAll [_ _] (throw (UnsupportedOperationException.)))
+  (retainAll [_ _] (throw (UnsupportedOperationException.)))
+  (size [_] (.count om))
+  (toArray [_] (clojure.lang.RT/seqToArray (keys om)))
 
   IHashEq
   (hasheq [this]
@@ -217,11 +217,11 @@
       _hash))
 
   IObj
-  (withMeta [this meta] (VecSet. (.withMeta om meta) _hash))
-  (meta [this] (.meta om))
+  (withMeta [_ meta] (VecSet. (.withMeta om meta) _hash))
+  (meta [_] (.meta om))
 
   IEditableCollection
-  (asTransient [this] (transient-ordered-set om))
+  (asTransient [_] (transient-ordered-set om))
 
   Object
   (toString [this] (clojure.lang.RT/printString this)))
@@ -241,8 +241,8 @@
 
 (deftype TransientVecSet [^TransientVecMap om]
   ITransientSet
-  (count [this] (.count ^ITransientMap om))
-  (get [this k] (.valAt ^ITransientMap om k))
+  (count [_] (.count ^ITransientMap om))
+  (get [_ k] (.valAt ^ITransientMap om k))
   (disjoin [this k]
     (let [nom (.without ^ITransientMap om k)]
       (if (identical? nom om)
@@ -253,15 +253,15 @@
       (if (identical? nom om)
         this
         (TransientVecSet. nom))))
-  (contains [this k]
+  (contains [_ k]
     (boolean (.valAt ^ITransientVector (:lst om) k)))  ;; ensures that nil members are reported correctly
-  (persistent [this]
+  (persistent [_]
     (VecSet. (.persistent ^TransientVecMap om) 0))
 
   IFn
   (invoke [this k] (.get ^ITransientSet this k))
-  (invoke [this k not-found] (.valAt ^ITransientMap om k not-found))
-  (invoke [this a b & rest] (throw (UnsupportedOperationException.)))
+  (invoke [_ k not-found] (.valAt ^ITransientMap om k not-found))
+  (invoke [_ _ _ & _] (throw (UnsupportedOperationException.)))
   (applyTo [this s] (case (count s)
                       1 (.get ^ITransientSet this (first s))
                       2 (.valAt ^ITransientMap om (first s) (second s))
@@ -270,3 +270,139 @@
 (defn transient-ordered-set
   [^IEditableCollection os]
   (TransientVecSet. (.asTransient os)))
+
+(declare transient-multi-map)
+
+(deftype MultiMap [m ^int _hash]
+  IFn
+  (invoke [_ k] (m k))
+  (invoke [_ k not-found] (m k not-found))
+  (invoke [_ _ _ & _] (throw (UnsupportedOperationException.)))
+  (applyTo [_ s] (case (count s)
+                   1 (m (first s))
+                   2 (m (first s) (second s))
+                   (throw (clojure.lang.ArityException. (count s) "Map.invoke"))))
+
+  IPersistentMap
+  (assoc [this k v]
+    (if-let [n (m k)]
+      (if (contains? n v)
+        this
+        (MultiMap. (assoc m k (conj n v)) 0))
+      (MultiMap. (assoc m k #{v}) 0)))
+  (assocEx [this k v]
+    (when-let [vs (contains? m k)]
+      (when (contains? vs v)
+        (throw (ex-info "Entry is already present" {:key k :value v}))))
+    (.assoc this k v))
+  (without [this entry]
+    (if-let [[k v] (cond
+                     (instance? Map$Entry entry) [(.getKey ^Map$Entry entry) (.getValue ^Map$Entry entry)]
+                     (instance? MapEntry entry) [(first entry) (second entry)]
+                     (and (vector? entry) (= 2 (count entry))) [(first entry) (second entry)])]
+      (if-let [vs (m k)]
+        (if (contains? vs v)
+          (let [vsm (disj vs v)]
+            (if (seq vsm)
+              (MultiMap. (assoc m k vsm) 0)
+              (MultiMap. (dissoc m k) 0)))
+          this)
+        this)
+      (MultiMap. (dissoc m entry) 0)))
+  (iterator [_] (.iterator ^Collection (for [[k vs] m v vs] (MapEntry/create k v))))
+  (containsKey [_ k] (contains? m k))
+  (entryAt [_ k] (.entryAt m k))
+  (count [_] (apply + (map count (vals m))))
+  (cons [this [k v]] (.assoc this k v))
+  (empty [_] (MultiMap. {} 0))
+  (equiv [this o]
+    (if (instance? IPersistentMap o)
+      (and (instance? MapEquivalence o) (APersistentMap/mapEquals this o))
+      (APersistentMap/mapEquals this o)))
+  (seq [_] (for [[k vs] m v vs] (MapEntry/create k v)))
+  (valAt [_ k] (m k))
+  (valAt [_ k not-found] (m k not-found))
+
+  IEditableCollection
+  (asTransient [_] (transient-multi-map m))
+
+  IHashEq
+  (hasheq [this]
+    (if (zero? _hash)
+      (set! (. this _hash) (clojure.lang.Murmur3/hashUnordered this))
+      _hash))
+
+  IObj
+  (withMeta [_ meta] (MultiMap. (.withMeta ^IObj m meta) _hash))
+  (meta [_] (.meta ^IObj m))
+
+  MapEquivalence
+
+  Map
+  (clear [_] (throw (UnsupportedOperationException.)))
+  (containsValue [_ v] (some #(contains? % v) (vals m)))
+  (equals [this o] (.equiv this o))
+  (get [_ k] (m k))
+  (hashCode [_] (+ magic (hash m)))
+  (isEmpty [_] (empty? m))
+  (keySet [_] (set (keys m)))
+  (size [this] (.count this))
+  (values [_] (apply concat (vals m)))
+  (put [_ _ _] (throw (UnsupportedOperationException.)))
+  (putAll [_ _] (throw (UnsupportedOperationException.)))
+  (remove [_ _] (throw (UnsupportedOperationException.)))
+
+  Object
+  (toString [this] (clojure.lang.RT/printString this)))
+
+(deftype TransientMultiMap [^ITransientMap m count*]
+  ITransientMap
+  (assoc [this k v]
+    (if-let [vs (get m k)]
+      (if (contains? vs v)
+        this
+        (TransientMultiMap. (assoc! m k (conj vs v)) (inc count*)))
+      (TransientVecMap. (assoc! m k #{v}) (inc count*))))
+  (without [this entry]
+    (if-let [[k v] (cond
+                     (instance? Map$Entry entry) [(.getKey ^Map$Entry entry) (.getValue ^Map$Entry entry)]
+                     (instance? MapEntry entry) [(first entry) (second entry)]
+                     (and (vector? entry) (= 2 (count entry))) [(first entry) (second entry)])]
+      (if-let [vs (get m k)]
+        (if (contains? vs v)
+          (let [vsm (disj vs v)]
+            (if (seq vsm)
+              (TransientMultiMap. (assoc! m k vsm) (dec count*))
+              (TransientMultiMap. (dissoc! m k) (dec count*))))
+          this)
+        this)
+      (if-let [vs (get m entry)]
+        (TransientMultiMap. (dissoc! m entry) (- count* (count vs)))
+        this)))
+  (valAt [_ key] (get m key))
+  (valAt [_ key not-found] (get m key not-found))
+  (count [_] count*)
+  (persistent [_] (MultiMap. (.persistent m) 0))
+  (conj [this [k v]] (.assoc this k v))
+
+  IFn
+  (invoke [this k] (.valAt this k))
+  (invoke [this k not-found] (.valAt this k not-found))
+  (invoke [_ _ _ & _] (throw (UnsupportedOperationException.)))
+  (applyTo [this s] (case (count s)
+                      1 (.valAt this (first s))
+                      2 (.valAt this (first s) (second s))
+                      (throw (clojure.lang.ArityException. (count s) "Map.invoke")))))
+
+(defn- transient-multi-map
+  [^IEditableCollection m]
+  (TransientMultiMap. (.asTransient m) (count m)))
+
+(def EMPTY_MULTI_MAP (MultiMap. {} 0))
+
+(defn multi-map
+  "Creates a map object that accepts multiple values per key"
+  ([] EMPTY_MULTI_MAP)
+  ([& keyvals]
+   (let [kvs (partition 2 keyvals)]
+     (into (transient EMPTY_MULTI_MAP) (map #(apply MapEntry/create %)) kvs))))
